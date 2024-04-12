45735b0dcef64b9cdee67a8f2c5d41b8d0b9454b
GALOPPE aurélien <agaloppe@me.com>
2024-04-12 10:11:52 +0200

    debug admin product destroy

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   belongs_to :category
   belongs_to :brand
   has_many :options, dependent: :destroy
-  has_one :service
+  has_one :service, dependent: :destroy
   has_and_belongs_to_many :rals
   has_and_belongs_to_many :motorists
   has_many_attached :images
