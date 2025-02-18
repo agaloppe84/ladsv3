e1c61173e3a444dbf1ffb23bf9cec7403c6011f7
GALOPPE aurélien <agaloppe@me.com>
2025-02-18 09:37:02 +0100

    add views to admin

--- app/controllers/admin/motorists_controller.rb
+++ app/controllers/admin/motorists_controller.rb
@@ -1,21 +1,30 @@
 class Admin::MotoristsController < AdminController
 
   def index
+    @motorists = Motorist.all
   end
 
   def show
-
+    @motorist = Motorist.find(params[:id])
   end
 
   def edit
+    @motorist = Motorist.find(params[:id])
   end
 
   def new
+    @motorist = Motorist.new
   end
 
   def create
   end
 
   def update
+  end
+
+  private
+
+  def motorist_params
+    params.require(:motorist).permit(:name, :logo)
   end
 end
