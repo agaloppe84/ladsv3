ebb8fbe7d99d45f742871e66b20c88a35e1a3eea
GALOPPE aurélien <agaloppe@me.com>
2025-03-17 15:25:09 +0100

    start new home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
     @event = Event.where(':date BETWEEN start_date AND end_date', date: DateTime.now).last
     @photos = Product.all.first.images
     @carousel_products = Product.all.first(6)
+    @carousel_products = []
     add_breadcrumb("Home", root_path)
   end
 
