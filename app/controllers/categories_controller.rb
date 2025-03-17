b2504b70cef159ac6577c3e22df230adb8d61d7b
GALOPPE aurélien <agaloppe@me.com>
2025-03-17 14:14:29 +0100

    add multiple doc on product show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Category.all
+    @categories = Category.all.order(:name)
     @event = Event.where(':date BETWEEN start_date AND end_date', date: DateTime.now).last
     add_breadcrumb("Home", root_path)
     add_breadcrumb("Produits", categories_path)
@@ -9,7 +9,7 @@ class CategoriesController < ApplicationController
   def show
     @categories = Category.all
     @category = Category.find(params[:id])
-    @products = @category.products.where(type: nil)
+    @products = @category.products.where(type: nil).order(:name)
     add_breadcrumb("Home", root_path)
     add_breadcrumb("Produits", categories_path)
     add_breadcrumb(@category.name, nil, @category.products.count)
