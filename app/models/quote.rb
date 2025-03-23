9629b83f0e6d879b13fea38718d292393476a5e9
GALOPPE aurélien <agaloppe@me.com>
2025-03-23 17:40:38 +0100

    remove websocket

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,3 @@
 class Quote < ApplicationRecord
   validates_presence_of :email, :lastname, :address, :city, :phone, :product, :message
-  after_create_commit { broadcast_prepend_to "admin_quotes" }
 end
