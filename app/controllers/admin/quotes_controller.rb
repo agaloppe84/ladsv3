6e3c00796882a3e62b5b7499866b0e2786f213e5
GALOPPE aurélien <agaloppe@me.com>
2025-03-24 13:53:32 +0100

    add processed to quotes

--- app/controllers/admin/quotes_controller.rb
+++ app/controllers/admin/quotes_controller.rb
@@ -1,11 +1,36 @@
 class Admin::QuotesController < AdminController
-
+  before_action :set_quote, only: [:update]
   def index
     @quotes = Quote.all.order(updated_at: :desc)
   end
 
   def show
     @quote = Quote.find(params[:id])
+  end
+
+  def update
+    if @quote.update(quote_params)
+      respond_to do |format|
+        format.html { redirect_to admin_quotes_path, notice: "Le devis a été mis à jour." }
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace("quote_#{@quote.id}",
+                                                     partial: "admin/quotes/quote_row",
+                                                     locals: { quote: @quote })
+        end
+      end
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_quote
+    @quote = Quote.find(params[:id])
+  end
+
+  def quote_params
+    params.require(:quote).permit(:processed)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_03_18_163042) do
+ActiveRecord::Schema[7.0].define(version: 2025_03_24_120353) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -135,6 +135,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_03_18_163042) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "processed", default: false
   end
 
   create_table "rals", force: :cascade do |t|
