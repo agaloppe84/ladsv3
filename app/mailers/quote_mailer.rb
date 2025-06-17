741f682d6cace1b872024a64b0dd567979a23465
GALOPPE aurélien <agaloppe@me.com>
2025-06-17 09:43:05 +0200

    add emoji in mail + add correct mail receivers

--- app/mailers/quote_mailer.rb
+++ app/mailers/quote_mailer.rb
@@ -3,8 +3,8 @@ class QuoteMailer < ApplicationMailer
   def new_quote
     @quote = params[:quote]
     mail(
-      to:      'agaloppe@me.com',
-      subject: "Nouvelle demande de devis de #{@quote.lastname}"
+      to: ['agaloppe@me.com', 'ads.pascale@lesartisansdustore.com'],
+      subject: "⚠️ Nouvelle demande de devis de #{@quote.lastname}"
     )
   end
 end
