e3caf7e657429e67317773e7d7c7491fc0c7db7c
GALOPPE aurélien <agaloppe@me.com>
2026-03-26 07:48:03 +0100

    remove useless variables

--- app/components/home_open_day_component.rb
+++ app/components/home_open_day_component.rb
@@ -1,17 +1,4 @@
 # frozen_string_literal: true
 
 class HomeOpenDayComponent < ViewComponent::Base
-  def initialize(categories:)
-    @categories = categories
-  end
-
-  private
-
-  attr_reader :categories
-
-  def all_category_names
-    @all_category_names ||= categories.filter_map do |category|
-      category.name.to_s.strip.presence
-    end.uniq
-  end
 end
